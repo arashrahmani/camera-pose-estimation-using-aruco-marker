65072e849a95a7309cd0ae71b82185a05435b147
ArashRahmani <arash.rahmani.999@gmail.com>
2019-12-22 20:03:11 -0500

    editing some properties of square structure

--- ArucoPosEstimation.h
+++ ArucoPosEstimation.h
@@ -1,23 +1,25 @@
+#ifndef ArucoposEstimation
+#define ArucoposEstimation
+
 #include <vector>
-#include <opencv4/opencv2/highgui.hpp>
-#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/core.hpp>
 using namespace std;
 using namespace cv;
 struct square{
   
-  // u is horizontal coordinate and v is vertical coordinate on the sensor for any point or (coordinates of pixels on the sensor to meter)
-  long double uA,vA,uB,vB,uC,vC,uD,vD;
-  long double u[4];
-  long double v[4];
+  // the position of Aruco corners on the sensor to meter (u,v)
+  Point2d imageCorners[4];
+  
+  // the position of Aruco corners in real world to meter (X,Y,Z) . The origin of world is the Center of lens (center of aperture in pinhole camera model)
+  Point3d realCorners[4];
+  
   // the square side lengh of arucoes -example: 0.1 (m)
   float length = 0.1;
-  
-  // k will be u/fX and c will be v/fY
-  long double k[4];
-  long double c[4];
-  
-  // r0 , r1 , r2 , r3 are real coordinates for A,B,C,D on the square with respect to the camera sensor or (coordinates of A,B,C,D in real 3D world with respect to camera lens) 
-  long double r[4][3];
+
+  // every vertex have a pair of (k , c) such that: k is u/fX and c is v/fY
+  double kc[4][2];
   
   int squareID;
 };
+
+#endif
